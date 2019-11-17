abe97f140be5f05064b96f6dca9e8179a0ccf6b3
liyucheng <1846427859@qq.com>
2019-11-17 21:11:53 +0800

    添加注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * springboot 结合mybatis的简单使用
+ * 配置文件如：application.properties
+ * 访问路径：http://localhost:8080/selectStudents
+ */
 @SpringBootApplication
 public class DemoApplication {
 
